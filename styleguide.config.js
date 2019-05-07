6111bf899caba2bf6b81dbaed8f8891befbf1728
Andrew Prifer <andrewpeterprifer@gmail.com>
2019-05-07 15:15:50 +0200

    Reproduce external stylesheet breaking styleguidist styles (section headers in this case)

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,18 @@
 module.exports = {
 	title: 'React Style Guide Example',
 	defaultExample: true,
+	require: ['semantic-ui-css/semantic.min.css'],
+	sections: [
+		{
+			name: 'Buttons',
+			content: 'docs/Buttons.md',
+			components: 'src/components/buttons/**/*.{js,jsx,ts,tsx}',
+		},
+		{
+			name: 'Misc',
+			components: 'src/components/misc/**/*.{js,jsx,ts,tsx}',
+		},
+	],
 	webpackConfig: {
 		module: {
 			rules: [
@@ -17,10 +29,17 @@ module.exports = {
 							loader: 'css-loader',
 							options: {
 								importLoaders: 1,
-								modules: true
-							}
+								modules: true,
+							},
 						},
 					],
+				},
+				{
+					test: /\.(eot|png|svg|[ot]tf|woff2?)(\?v=\d+\.\d+\.\d+)?$/,
+					use: {
+						loader: 'url-loader',
+						options: { limit: 10000 },
+					},
 				},
 			],
 		},
